8989c6a9222867ea19b1db2619b68b9b05611891
pigbog <mesteves@gmail.com>
2019-02-13 22:20:38 +0000

    Can now send and receive data from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
+
 import React, { Component } from 'react';
 import Chatbar from './Chatbar.jsx';
 import MessageList from './MessageList.jsx';
 import Message from './Message.jsx';
 
+
 class App extends Component {
   constructor(props) {
+    console.log('Connected to server.') 
+  
     super(props);
     this.state = {
       currentUser: { name: "Anonymous" },
       currId:0,
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: "glumpus"
-        },
-        {
-          username: "Glorbus",
-          content: "You cretin, you absolute buffoon. How would we see your marbles? This is a chatroom.",
-          id: "doorf"
-        },
-        {
-          username: "Glorbus",
-          content: "I hope you're stricken with an incurable disease.",
-          id: "gadoorf"
-        }
-      ]
+      messages: []
     };
+
     this.getMessage= this.getMessage.bind(this);
     this.getUsername= this.getUsername.bind(this);
+
 
   }
   getMessage(event){
   if (event.key === 'Enter'){
     console.log(event.target.value)
     let newMessage= {id:this.state.currId, username:this.state.currentUser.name, content: event.target.value };
+    let sendData = { username: newMessage.username, content: newMessage.content}
+    this.socket.send(JSON.stringify(sendData));
     const messages= this.state.messages.concat(newMessage);
-    this.setState({messages: messages, currId: this.state.currId+1})
+    this.setState({messages: messages})
 
     }
   };
@@ -46,16 +38,9 @@ class App extends Component {
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: "doorbulon", username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
-  };
+    this.socket = new WebSocket("ws:0.0.0.0:3001"); 
+    this.socket.onopen = () => {}
+    };
   
   render() {
     return (
